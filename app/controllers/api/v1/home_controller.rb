d8996f72dfb8d844baffe962948f35075d1294d8
Rohitrox <link2j.roy@gmail.com>
2013-04-24 23:04:34 +0545

    return 0 if no location params

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -7,9 +7,10 @@ class Api::V1::HomeController <  Api::V1::BaseController
 
   def index
     @r_hash = OpenStruct.new({message: "We could could find you. Please login."}) unless @user
+    near_by_count = params[:latt].present? ? Event.near([params[:latt],params[:longt]], 1).size : 0
     @r_hash = OpenStruct.new({
       upcoming_count: Event.upcoming.size,
-      near_by_count: Event.near([params[:latt],params[:longt]], 1).size,
+      near_by_count: near_by_count,
       featured_count: Event.featured.size,
       interest_in_count: Event.where(:category.in => params[:likes]).size
       })
